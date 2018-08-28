3a67c8d2bcc1131743e6c26cb391fde5e5de99a2
asabogal <asabogal.git@gmail.com>
2018-08-28 09:20:42 -0400

    application controller initial setup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,26 @@ require './config/environment'
 class ApplicationController < Sinatra::Base
 
   configure do
+    enable :session
+    set :session_secret, "recordcharts"
     set :public_folder, 'public'
     set :views, 'app/views'
   end
 
   get "/" do
     erb :welcome
+  end
+
+  helpers do
+
+    def logged_in?
+      !!session[:user_id]
+    end
+
+    def current_user
+     User.find(session[:user_id])
+   end
+
   end
 
 end
