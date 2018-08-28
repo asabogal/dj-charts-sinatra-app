cc56809b747e0b57493f82522196bd2fb0c21862
asabogal <asabogal.git@gmail.com>
2018-08-28 00:15:04 -0400

    add slug methods to record model

--- app/models/record.rb
+++ app/models/record.rb
@@ -2,4 +2,12 @@ class Record < ActiveRecord::Base
   belongs_to :chart
   belongs_to :user
 
+  def slug
+    self.title.gsub(" ","-").downcase
+  end
+
+  def self.find_by_slug(slug)
+    self.all.find {|record| record.slug == slug}
+  end
+
 end
