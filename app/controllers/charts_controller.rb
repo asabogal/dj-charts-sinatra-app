cf81317e0dffd03d3b80e7d4737a7b981abce39a
asabogal <asabogal.git@gmail.com>
2018-08-29 02:12:33 -0400

    setup preliminary chart creation

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -1,5 +1,21 @@
 class ChartsController < ApplicationController
 
+  get '/charts' do
+    erb :"/charts/index"
+  end
 
+  get '/charts/new' do
+    if logged_in?
+      erb :"/charts/new_chart"
+    else
+    #add flash message?
+      redirect "/login"
+    end
+  end
+
+  post '/charts' do
+    binding.pry
+    redirect "/charts"
+  end
 #---
 end
