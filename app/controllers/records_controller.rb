f624a60bf3c64cae36620287e786faadd23a888b
asabogal <asabogal.git@gmail.com>
2018-08-29 15:05:43 -0400

    add index path action

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,5 +1,17 @@
 class RecordsController < ApplicationController
 
+  # need paths:
+    # get /records/<%=record.id%>/edit
 
+    #post /records/delete" do
+      # record.id.delete ?
+    # end
+
+    get '/records' do
+      @records = Record.all
+      erb :"/records/index"
+    end
+
+    
 #---
 end
