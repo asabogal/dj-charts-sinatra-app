b9f9b4a7364c73cf3d923c7dffb18ffe8ca4baf0
asabogal <asabogal.git@gmail.com>
2018-08-29 21:23:27 -0400

    add edit path and patch action

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,17 +1,37 @@
 class RecordsController < ApplicationController
 
-  # need paths:
-    # get /records/<%=record.id%>/edit
 
-    #post /records/delete" do
-      # record.id.delete ?
-    # end
-
-    get '/records' do
-      @records = Record.all
-      erb :"/records/index"
+    get '/records/:id/edit' do
+      if logged_in? && current_user
+        @record = Record.find_by_id(params[:id])
+        erb :"/records/edit_record"
+      else
+        flash[:notice] = "Please log in to do that"
+        redirect "/login"
+      end
     end
 
-    
+    get '/records' do
+        @records = Record.all
+        erb :"/records/index"
+    end
+
+    patch '/records/:id' do
+      if logged_in? && current_user
+        @record = Record.find_by_id(params[:id])
+          if @record.update(title: params[:record][:title], artist: params[:record][:artist], label: params[:record][:label])
+            @record.save
+            @chart = Chart.find_by_id(@record.chart_id)
+            redirect "/charts/#{@chart.slug}"
+          else
+            flash[:notice] = "Please fill in all fields"
+            redirect "/records/#{@record.id}/edit"
+          end
+        else
+          flash[:notice] = "Please log in to do that"
+          redirect "/login"
+      end
+    end
+
 #---
 end
