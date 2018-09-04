f6a798b6a4e1c794f874b340656863f54824bd01
asabogal <asabogal.git@gmail.com>
2018-09-04 17:15:25 -0400

    fix email format validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,8 +10,7 @@ class User < ActiveRecord::Base
   validates :email, uniqueness: true
   validates :password, presence: true
 
-
-  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
 
   def slug
     self.username.gsub(" ","-").downcase
